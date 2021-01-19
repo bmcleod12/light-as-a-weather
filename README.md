2bb99bba2889ca41cadcc8cd5b8a9d4d9a7daa39
Brenna McLeod <brenna.mcleod94@gmail.com>
2021-01-19 09:40:06 -0600

    updated js to call two APIs and updated HTML to display mock up layout

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # light-as-a-weather
 
+https://bmcleod12.github.io/light-as-a-weather/
+
+
 # 06 Server-Side APIs: Weather Dashboard
 
 Developers are often tasked with retrieving data from another application's API and using it in the context of their own. Third-party APIs allow developers to access their data and functionality by making requests with specific parameters to a URL. Your challenge is to build a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS.
--- script.js
+++ script.js
@@ -1,23 +1,60 @@
 $(document).ready(function(){
 
+    $("#search-city").keydown(function (event) {
+    if (event.keyCode == 13) {
+        event.preventDefault();
+    }});
+
     $("#search").on("click", function(event) {
         event.preventDefault();
 
         var q = $("#search-city").val();
         console.log(q);
 
-        var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + q + "&units=imperial" + "&appid=c8dda36408a8eec1bd2b3381270f38a9";
+        // var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + q + "&units=imperial" + "&appid=c8dda36408a8eec1bd2b3381270f38a9";
 
         $.ajax({
-        url: queryURL,
+        url: "https://api.openweathermap.org/data/2.5/forecast?q=" + q + "&units=imperial" + "&appid=c8dda36408a8eec1bd2b3381270f38a9",
         method: "GET"
         }).then(function(response) {
-            console.log(response);
-            console.log(response.city.name);
-            console.log(response.list[0].main.temp);
-            console.log(response.list[0].main.humidity);
-            console.log(response.list[0].weather[0].icon);
-            console.log(response.list[0].weather[0].main);
+
+            var latitude = response.city.coord.lat;
+            var longitude = response.city.coord.lon;
+
+            // console logs to double check
+            // console.log(response);
+            // console.log(response.city.name);
+            // console.log("Latitude: " + latitude);
+            // console.log("Longitude: " + longitude);
+            // console.log(response.list[0].main.temp);
+            // console.log(response.list[0].main.humidity);
+            // console.log(response.list[0].weather[0].icon);
+            // console.log(response.list[0].weather[0].main);
+
+            
+            $.ajax({
+                url: "http://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=minutely,hourly,alerts&units=imperial&appid=c8dda36408a8eec1bd2b3381270f38a9",
+                method: "GET"
+                }).then(function(response) { 
+                    console.log(response);
+
+                    // var currentHumidity = 
+                    console.log("Humidity: " + response.current.humidity);
+                    console.log("Current Temperature: " + response.current.temp);
+                    console.log("UV Index: " + response.current.uvi);
+                    console.log("Wind Speed: " + response.current.wind_speed);
+                    console.log("Current Description: " + response.current.weather[0].main);
+                    console.log("Icon ID: " + response.current.weather[0].icon);
+
+
+                    // after I grab the data, create everything for the Current card and append to #current-weather-card
+
+                    // <img src="http://openweathermap.org/img/wn/13n@2x.png" width="50px" height="50px" alt=""></img>
+                    // "http://openweathermap.org/img/wn/" + iconID + "@2x.png"
+                });
+
+
+
         });
         
     });
@@ -26,7 +63,7 @@ $(document).ready(function(){
 });
 
 
-// api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+// https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
 
 
 // $("#select-artist").on("click", function(event) {
