fe04efcb1836df2f62fed06a9d97b67cc4ba347d
Brenna McLeod <brenna.mcleod94@gmail.com>
2021-01-21 10:31:49 -0600

    partially complete updating the left panel to show searched cities; javascript to match

--- script.js
+++ script.js
@@ -1,26 +1,102 @@
+var cityList = [];
+
 $(document).ready(function(){
 
+    // Prevents the enter key from submitting the form and allows it to call the API
     $("#search-city").keydown(function (event) {
     if (event.keyCode == 13) {
         event.preventDefault();
+        callAPI();
     }});
 
+    // Listens for the click on the search icon and calls the API
     $("#search").on("click", function(event) {
         event.preventDefault();
+        callAPI();
+    });
+        
+    function callAPI() {
 
+        // grabs the searched city term
         var q = $("#search-city").val();
-        console.log(q);
-
-        // var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + q + "&units=imperial" + "&appid=c8dda36408a8eec1bd2b3381270f38a9";
-
+        
         $.ajax({
         url: "https://api.openweathermap.org/data/2.5/forecast?q=" + q + "&units=imperial" + "&appid=c8dda36408a8eec1bd2b3381270f38a9",
         method: "GET"
         }).then(function(response) {
-
+         
+            // grabs latitude and longitude for use the next API call; create cityName so Capitalization is corrected if incorrect in the search
             var latitude = response.city.coord.lat;
             var longitude = response.city.coord.lon;
+            var cityName = response.city.name;
 
+            // adds cityName to the cityList array
+            cityList.push(cityName);
+            
+            // clears search input
+            $("#search-city").val("");
+            
+            renderCityList();
+
+            $.ajax({
+                url: "http://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=minutely,hourly,alerts&units=imperial&appid=c8dda36408a8eec1bd2b3381270f38a9",
+                method: "GET"
+                }).then(function(response) { 
+
+                    // references HTML IDs to display API data on screen
+                    $("#current-city-header").text(cityName+ ", " + response.current.weather[0].main).append($("<img>").attr("src", "http://openweathermap.org/img/wn/" + response.current.weather[0].icon + "@2x.png").attr("width","60px").attr("height", "60px"));
+                    $("#current-temp").text(response.current.temp);
+                    $("#current-humidity").text(response.current.humidity);
+                    $("#current-wind-speed").text(response.current.wind_speed);
+                    
+                    // declares UV Index as a variable for easy reference below
+                    var currentUVIndex = response.current.uvi;
+
+                    // checks if current UV Index is 0-2, 3-5, 6-7, 8-10, or 11+; based on EPA's UV Index scale: https://www.epa.gov/sunsafety/uv-index-scale-0
+                    if(currentUVIndex < 3) {
+                        $("#uv-index").addClass("badge bg-success").text(currentUVIndex);
+                    }
+
+                    if(currentUVIndex >= 3 && currentUVIndex < 6) {
+                        $("#uv-index").addClass("badge bg-warning").text(currentUVIndex);
+                    }
+                    
+                    if(currentUVIndex >= 6 && currentUVIndex < 8) {
+                        $("#uv-index").addClass("badge").css("background-color", "rgb(247, 112, 1)").text(currentUVIndex);
+                    }
+
+                    if(currentUVIndex >= 8 && currentUVIndex < 11) {
+                        $("#uv-index").addClass("badge bg-danger").text(currentUVIndex);
+                    }
+          
+                    if(currentUVIndex > 11) {
+                        $("#uv-index").addClass("badge").css("background-color", "purple").text(currentUVIndex);
+                    }
+                });
+        });
+    };
+
+
+    function renderCityList() {
+
+        // clears the left panel before creating an element for each object in the cityList array
+        $("#searched-cities").empty();
+
+        for (var i = 0; i < cityList.length; i++) {
+
+            var city = cityList[i];
+
+            var searchedCity = $("<li>").text(city);
+
+            $("#searched-cities").append(searchedCity);
+            
+        }
+    }
+
+
+});
+
+// CONSOLE LOGS FOR THE DATA CALL
             // console logs to double check
             // console.log(response);
             // console.log(response.city.name);
@@ -31,52 +107,11 @@ $(document).ready(function(){
             // console.log(response.list[0].weather[0].icon);
             // console.log(response.list[0].weather[0].main);
 
-            
-            $.ajax({
-                url: "http://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=minutely,hourly,alerts&units=imperial&appid=c8dda36408a8eec1bd2b3381270f38a9",
-                method: "GET"
-                }).then(function(response) { 
-                    console.log(response);
-
-                    // var currentHumidity = 
-                    console.log("Humidity: " + response.current.humidity);
-                    console.log("Current Temperature: " + response.current.temp);
-                    console.log("UV Index: " + response.current.uvi);
-                    console.log("Wind Speed: " + response.current.wind_speed);
-                    console.log("Current Description: " + response.current.weather[0].main);
-                    console.log("Icon ID: " + response.current.weather[0].icon);
-
-
-                    // after I grab the data, create everything for the Current card and append to #current-weather-card
-
-                    // <img src="http://openweathermap.org/img/wn/13n@2x.png" width="50px" height="50px" alt=""></img>
-                    // "http://openweathermap.org/img/wn/" + iconID + "@2x.png"
-                });
-
-
-
-        });
-        
-    });
-
-
-});
-
-
-// https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
-
-
-// $("#select-artist").on("click", function(event) {
-//     // Preventing the button from trying to submit the form
-//     event.preventDefault();
-//     // Storing the artist name
-//     var inputArtist = $("#artist-input").val().trim();
-
-//     // Running the searchBandsInTown function(passing in the artist as an argument)
-//     searchBandsInTown(inputArtist);
-//   });
-
-// Searches for city with api key listed below
-
-// extra key for current weather
-// b49a7ab043699d21844dd636541d9d32
+// CONSOLE LOGS FOR THE ONE CALL
+            // var currentHumidity = 
+            // console.log("Humidity: " + response.current.humidity);
+            // console.log("Current Temperature: " + response.current.temp);
+            // console.log("UV Index: " + response.current.uvi);
+            // console.log("Wind Speed: " + response.current.wind_speed);
+            // console.log("Current Description: " + response.current.weather[0].main);
+            // console.log("Icon ID: " + response.current.weather[0].icon);
