ab310f8e1dcce92fb86503eb304c4764b4cfec99
Brenna McLeod <brenna.mcleod94@gmail.com>
2021-01-23 15:07:19 -0600

    cleaned up javascript and updated search list to move the last clicked city to the front of the array

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ var cityList = [];
 
 $(document).ready(function(){
 
-    init();
-
     // Prevents the enter key from submitting the form and allows it to call the API
     $("#search-city").keydown(function (event) {
     if (event.keyCode == 13) {
@@ -17,30 +15,38 @@ $(document).ready(function(){
         callAPI();
     });
 
+    // assigns a listener to each list item and tells it to move that city to the front of the arry and place that city in the search bar before calling the api
+    $(document).on("click", "li", function (event) {
+        event.preventDefault();
+        $("#search-city").val(this.innerHTML);
+        console.log(cityList.indexOf(this.innerHTML));
+        if (cityList.indexOf(this.innerHTML) > 0) {
+            cityList.splice(cityList.indexOf(this.innerHTML), 1);
+            cityList.unshift(this.innerHTML);
+        }
+        callAPI();
+    });
+
     // clears the city list and refreshes the page
     $("#clearSearch").on("click", function(event) {
         event.preventDefault();
         window.localStorage.clear();
         location.reload();
     });
-
-    // this only works for one click
-    $("li").on("click", function (event) {
-            event.preventDefault();
-            // console.log("clicked");
-            $("#search-city").val(this.innerHTML);
-            callAPI();
-            // location.reload();
-    });
         
     function callAPI() {
-
         // grabs the searched city term
         var q = $("#search-city").val();
         
         $.ajax({
         url: "https://api.openweathermap.org/data/2.5/forecast?q=" + q + "&units=imperial" + "&appid=c8dda36408a8eec1bd2b3381270f38a9",
-        method: "GET"
+        method: "GET",
+        // presents user with an error if they don't search a real city/if it has typos/etc
+        error:function (xhr, ajaxOptions, thrownError){
+            if(xhr.status==404) {
+                alert("No cities were found that match your search. Please try again.");
+            }
+        }
         }).then(function(response) {
          
             // grabs latitude and longitude for use the next API call; create cityName so Capitalization is corrected if incorrect in the search
@@ -61,7 +67,6 @@ $(document).ready(function(){
                 url: "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=minutely,hourly,alerts&units=imperial&appid=c8dda36408a8eec1bd2b3381270f38a9",
                 method: "GET"
                 }).then(function(response) { 
-                    console.log(response);
                     $("#forecast-wrapper").empty();
 
                     // references HTML IDs to display API data on screen
@@ -94,93 +99,65 @@ $(document).ready(function(){
                         $("#uv-index").addClass("badge").css("background-color", "purple").text(currentUVIndex);
                     }
 
-                    // 5 day forecast element creation
-
+                    // 5 day forecast element creation. loops through days 1-5 in the response array and creates a card for each with the day and data
                     for(var i = 1; i < 6; i++) {
                         
-
                         var futureWeather = $("<p>").text(response.daily[i].weather[0].main).append($("<img>").attr("src", "https://openweathermap.org/img/wn/" + response.daily[i].weather[0].icon + "@2x.png").attr("width","35px").attr("height", "35px"));
                         var futureTemp = $("<p>").text("Temp: " + response.daily[i].temp.day + "°F");
                         var futureHumidity = $("<p>").text("Humidity: " + response.daily[i].humidity + "%"); 
 
+                        // gets the date for the next 5 days
+                        var fullDate = new Date()       
+                        var twoDigitMonth = ((fullDate.getMonth().length+1) === 1)? (fullDate.getMonth()+1) : '0' + (fullDate.getMonth()+1);
+                        var nextDay = twoDigitMonth + "/" + (fullDate.getDate()+i) + "/" + fullDate.getFullYear();
+
                         $("#forecast-wrapper").append($("<div>").addClass("col-auto").
                             append($("<div>").addClass("card mb-4").
-                            append($("<h5>").addClass("card-header bg-info").css("color", "white").text("Day " + i)).
+                            append($("<h5>").addClass("card-header bg-info").css("color", "white").text(nextDay)).
                             append($("<div>").addClass("card-body").append(futureWeather, futureTemp, futureHumidity))));
                     }
-
                 });
         });
     };
 
+    // stores the cities for later
+    function storeCityList() {
+        localStorage.setItem("cityList", JSON.stringify(cityList));
+    }
+
+    // displays the search city list in the left panel
+    function renderCityList() {
+        // clears the left panel before creating an element for each object in the cityList array
+        $("#searched-cities").empty();
+
+        // loops through cities that have been searched and makes the first city in the list a darker green
+        for (var i = 0; i < cityList.length; i++) {
+            var city = cityList[i];
+            var searchedCity = $("<li>").attr("id", i + "-storedCity").addClass("list-group-item list-group-item-action list-group-item-success").text(city);
+            $("#0-storedCity").addClass("active");
+            $("#searched-cities").append(searchedCity);
+            
+        }
+    }
+
+    // looks for city list in storage and presents the current date on screen
     function init() {
         var storedCityList = JSON.parse(localStorage.getItem("cityList"));
-        console.log("init: " + storedCityList);
-
         if (storedCityList !== null) {
             cityList = storedCityList;
         }
 
         renderCityList();
 
-        // stopped here for now
-        var fullDate = new Date()
-        console.log(fullDate);
-         
+        var fullDate = new Date()       
         //convert month to 2 digits
         var twoDigitMonth = ((fullDate.getMonth().length+1) === 1)? (fullDate.getMonth()+1) : '0' + (fullDate.getMonth()+1);
-         
-        var currentDate = fullDate.getDate() + "/" + twoDigitMonth + "/" + fullDate.getFullYear();
-        console.log(currentDate);
-        //19/05/2011
+        var currentDate = twoDigitMonth + "/" + fullDate.getDate() + "/" + fullDate.getFullYear();
+        $("#current-date").text(currentDate);
 
     }
 
-    function storeCityList() {
-        localStorage.setItem("cityList", JSON.stringify(cityList));
-        console.log("local storage: " + cityList);
-    }
-
-    function renderCityList() {
-        // clears the left panel before creating an element for each object in the cityList array
-        $("#searched-cities").empty();
-
-        for (var i = 0; i < cityList.length; i++) {
-
-            var city = cityList[i];
-
-            var searchedCity = $("<li>").attr("id", i + "-storedCity").addClass("list-group-item list-group-item-action list-group-item-success").text(city);
-
-            $("#0-storedCity").addClass("active");
-
-            $("#searched-cities").append(searchedCity);
-            
-        }
-    }
-
+    // runs the init function upon page load
+    init();
 
 });
-
-// CONSOLE LOGS FOR THE DATA CALL
-            // console logs to double check
-            // console.log(response);
-            // console.log(response.city.name);
-            // console.log("Latitude: " + latitude);
-            // console.log("Longitude: " + longitude);
-            // console.log(response.list[0].main.temp);
-            // console.log(response.list[0].main.humidity);
-            // console.log(response.list[0].weather[0].icon);
-            // console.log(response.list[0].weather[0].main);
-
-// CONSOLE LOGS FOR THE ONE CALL
-            // var currentHumidity = 
-            // console.log("Humidity: " + response.current.humidity);
-            // console.log("Current Temperature: " + response.current.temp);
-            // console.log("UV Index: " + response.current.uvi);
-            // console.log("Wind Speed: " + response.current.wind_speed);
-            // console.log("Current Description: " + response.current.weather[0].main);
-            // console.log("Icon ID: " + response.current.weather[0].icon);
-            // console.log(response.daily[1].temp.day);
-            // console.log(response.daily[1].humidity);
-            // console.log(response.daily[1].weather[0].main);
-            // console.log(response.daily[1].weather[0].icon);
